9c9553213c4ca9cd3ffebc5299fa36bf722774a7
hit patel <21004500210031@ljku.edu.in>
2024-07-11 17:54:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 echo "${KEY}" > key.pem
 chmod 400 key.pem
+
 scp -o StrictHostKeyChecking=no -i key.pem -r build/* ${USERNAME}@${HOST}:/var/www/html/
+
 ssh -o StrictHostKeyChecking=no -i key.pem ${USERNAME}@${HOST} << 'EOF'
-  # Install Node.js and npm if not already installed
+  cd /var/www/html
   if ! command -v npm &> /dev/null
   then
     curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
     sudo apt-get install -y nodejs
   fi
-  
-  cd /var/www/html
   npm install --production
-  npm run start
-EOF
