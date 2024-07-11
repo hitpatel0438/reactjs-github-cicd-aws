d1a13cf88959e85ff9090360d6b6108689ad6aef
hit patel <21004500210031@ljku.edu.in>
2024-07-11 17:47:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,14 @@ echo "${KEY}" > key.pem
 chmod 400 key.pem
 scp -o StrictHostKeyChecking=no -i key.pem -r build/* ${USERNAME}@${HOST}:/var/www/html/
 ssh -o StrictHostKeyChecking=no -i key.pem ${USERNAME}@${HOST} << 'EOF'
-  cd /var/www/html
-
-  # Check if Node.js and npm are installed, and install them if not
-  if ! command -v node &> /dev/null
-  then
-    echo "Node.js is not installed. Installing..."
-    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-  else
-    echo "Node.js is already installed."
-  fi
-
+  # Install Node.js and npm if not already installed
   if ! command -v npm &> /dev/null
   then
-    echo "npm is not installed. Installing..."
-    sudo apt-get install -y npm
-  else
-    echo "npm is already installed."
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt-get install -y nodejs
   fi
-
-  # Install dependencies and start the application
+  
+  cd /var/www/html
   npm install --production
   npm run start
 EOF
