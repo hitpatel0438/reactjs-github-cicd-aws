c06968c3de7b8aeba00974860530eb6f3a51ef84
hit patel <133954978+hitpatel0438@users.noreply.github.com>
2024-07-11 18:21:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
 
-# Navigate to your project directory
-cd /var/www/html
-
-# Pull the latest code from the main branch
-git pull origin main
-
-# Install dependencies
-npm install
-
-# Build the React project
-npm run build
-
-# (Optional) Restart your server if needed, e.g., using PM2
-# pm2 restart your-app-name
