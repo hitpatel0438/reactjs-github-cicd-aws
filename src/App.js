bb807e3f5af0ad333f8cd42af0a6520694a081ad
hit patel <133954978+hitpatel0438@users.noreply.github.com>
2024-07-19 18:54:09 +0530

    Update App.js KAN-1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import VideoPlayer from './VideoPlayer';
+
+const App = () => {
+  return (
+      <div className="App">
+          <h1>My Video Player</h1>
+          <VideoPlayer videoUrl="https://shavetape.cash/v/VaA3We4xbltKBgZ/Men.In.Black.International.2019.1080p.10Bit.BluRay.Hindi.English.ESubs.MoviesHeist.Org.mkv.mp4" />
+          <VideoPlayer videoUrl="https://shavetape.cash/v/yPa9x1mlW3C18aV/Fifty.Shades.Of.Grey.2015.1080p.10Bit.BRRip.Hindi.English.Esubs.Moviesmod.Com.mkv.mp4" />
+          <VideoPlayer videoUrl="https://shavetape.cash/v/yPa9x1mlW3C18aV/Fifty.Shades.Of.Grey.2015.1080p.10Bit.BRRip.Hindi.English.Esubs.Moviesmod.Com.mkv.mp4" />
+      </div>
+  );
+};
+
+export default App;
