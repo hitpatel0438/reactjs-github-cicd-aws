bee46dd3877dee5265cee9057a2abfb3a93294c8
hit patel <21004500210031@ljku.edu.in>
2024-07-20 19:20:08 +0530

    Update some changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  console.log(screen.debug()); // Output the rendered HTML for debugging
+  const linkElement = screen.getByText(/learn react/i); // Regex to match the text
   expect(linkElement).toBeInTheDocument();
 });
